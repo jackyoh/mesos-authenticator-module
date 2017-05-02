66090d9b1dea1eec96538a05e2b648b73259cb52
jack <jack@is-land.com.tw>
2017-05-02 03:40:31 -0400

    Modify README.md

--- README.sh
+++ README.sh
@@ -1,6 +1,7 @@
 Build module command
+
 ```
-g++ -std=gnu++11 -lmesos -fpic -c test_authentication_modules.cpp test_http_authenticator_module.cpp
+# g++ -std=gnu++11 -lmesos -fpic -c test_authentication_modules.cpp test_http_authenticator_module.cpp
 
 # g++ -shared -o libauthen.so.1.0.0 test_authentication_modules.o test_http_authenticator_module.o
 ```
